2db3a1dde71433bdfb8de607975a8ad390cbc323
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-02-15 00:23:47 +0530

    Delete src directory

--- src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getaiContent } from "../controllers/ai.controller.js";
-
-const router = Router(); // Create a new router
-
-router.route("/get-response").post(getaiContent); // Handle GET requests to "/get-response"
-
-export default router; // Export the router
