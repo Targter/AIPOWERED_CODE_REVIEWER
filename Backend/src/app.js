bc0af5dbfd43d03a6c1e0a6edf1d3cd126187ca8
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-02-15 02:07:28 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,8 +12,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "10mb" })); // Increase limit for JSON payloads
-app.use(express.urlencoded({ limit: "10mb", extended: true })); // Increase limit for URL-encoded data
+app.use(express.json({ limit: "50mb" })); // Increase limit for JSON payloads
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase limit for URL-encoded data
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
